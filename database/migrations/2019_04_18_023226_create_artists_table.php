d432f963589aaa62946cce4ee33fe0880e0bf2a1
Nguyen Huu Dat <sr.nguyenhuudat@gmail.com>
2019-04-18 16:05:58 +0700

    ref task#10823 migration

--- database/migrations/2019_04_18_023226_create_artists_table.php
+++ database/migrations/2019_04_18_023226_create_artists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateArtistsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('artists', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('avatar', 150)->nullable();
-            $table->rememberToken();
+            $table->string('name', 150);
+            $table->string('slug');
+            $table->string('avatar')->nullable();
+            $table->tinyInteger('year_active')->nullable()->unsigned();
+            $table->text('info')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('artists');
     }
 }
--- database/migrations/2019_04_18_024134_create_comments_table.php
+++ database/migrations/2019_04_18_024134_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('avatar', 150)->nullable();
-            $table->rememberToken();
+            $table->unsignedInteger('track_id')->nullable();
+            $table->unsignedInteger('album_id')->nullable();
+            $table->text('content');
+            $table->unsignedInteger('user_id');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
