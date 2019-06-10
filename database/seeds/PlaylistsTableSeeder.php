d8ec5b319961b80c848f6ae9755a2e07e0cbdb87
Nguyen Huu Dat <sr.nguyenhuudat@gmail.com>
2019-06-10 09:34:10 +0700

    deploy heroku

--- database/seeds/PlaylistsTableSeeder.php
+++ database/seeds/PlaylistsTableSeeder.php
@@ -14,22 +14,19 @@ class PlaylistsTableSeeder extends Seeder
     {
         factory(Playlist::class, 50)->create()->each(function ($playlist) {
            $tracks_id = [];
-           for ($i = 0; $i <= rand(0, 4); $i++) {
+           for ($i = 0; $i <= rand(0, 3); $i++) {
                switch ($i) {
                    case 0:
-                       $tracks_id[] = rand(1, 500);
+                       $tracks_id[] = rand(1, 200);
                        break;
                    case 1:
-                       $tracks_id[] = rand(501, 1000);
+                       $tracks_id[] = rand(501, 400);
                        break;
                    case 2:
-                       $tracks_id[] = rand(1001, 1500);
+                       $tracks_id[] = rand(401, 600);
                        break;
                    case 3:
-                       $tracks_id[] = rand(1501, 2000);
-                       break;
-                   case 4:
-                       $tracks_id[] = rand(2001, 2500);
+                       $tracks_id[] = rand(601, 800);
                        break;
                    default:
                        $tracks_id[] = rand(2501, 3000);
